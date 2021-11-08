17cace47500f7d65770b3a5bbbc8f75ec2f1c1b6
FonDitbul <niki7084@naver.com>
2021-11-09 00:37:43 +0900

    postgre, typeorm 설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ChatsModule } from './chats/chats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ChatsModule],
+  imports: [ChatsModule, TypeOrmModule.forRoot(), UsersModule],
   controllers: [AppController],
   providers: [],
 })
