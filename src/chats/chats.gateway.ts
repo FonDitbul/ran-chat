5e99e3859288dc1396bc03767a73238c3aaacb22
FonDitbul <niki7084@naver.com>
2021-11-12 18:09:07 +0900

    chats gateway 입장, 퇴장, 채팅 내역 database 저장 코드

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -8,49 +8,72 @@ import {
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
-import { Logger } from '@nestjs/common';
-
+import { HttpException, Logger } from '@nestjs/common';
+import { ChatRepository } from './chats.repository';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { chatEntity } from './entities/history-chat.entity';
 @WebSocketGateway({ namespace: 'chats' })
 export class ChatsGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-  constructor() {}
+  constructor(private readonly chatRepository: ChatRepository) {}
   private logger = new Logger('chat');
 
-  handleConnection(@ConnectedSocket() socket: Socket) {
-    this.logger.log(`connected : ${socket.id}`);
+  async handleConnection(@ConnectedSocket() socket: Socket) {
+    // this.logger.log(`connected : ${socket.id}`);
+    await this.handleUserEntry(socket);
   }
 
   async handleDisconnect(@ConnectedSocket() socket: Socket) {
-    this.logger.log(`disconnected : ${socket.id}`);
+    // this.logger.log(`disconnected : ${socket.id}`);
+    await this.handleUserExit(socket);
   }
 
   afterInit(server: any): any {
-    this.logger.log('init');
+    // this.logger.log('init');
   }
 
-  @SubscribeMessage('new_user')
-  async handleNewUser(
-    @MessageBody() temp: string,
-    @ConnectedSocket() socket: Socket,
-  ) {
-    const username = 'tempuser';
-    socket.broadcast.emit('new_user', username);
-    return username;
+  @SubscribeMessage('user_entry')
+  async handleUserEntry(@ConnectedSocket() socket: Socket) {
+    const newUserEntry = new chatEntity();
+    newUserEntry.username = socket.id;
+    newUserEntry.text = '님이 입장하셨습니다.';
+
+    await this.chatRepository.save(newUserEntry).catch((error) => {
+      throw new HttpException('Server ERROR', error);
+    });
+    socket.broadcast.emit('new_user', socket.id);
   }
 
-  @SubscribeMessage('user_info')
-  async handleChats(@MessageBody() data, @ConnectedSocket() socket: Socket) {
-    // socket.broadcast.emit('new_user', 'heyhey');
-    return 'Hello world!';
+  @SubscribeMessage('user_exit')
+  async handleUserExit(@ConnectedSocket() socket: Socket) {
+    const newUserDisconnect = new chatEntity();
+    newUserDisconnect.username = socket.id;
+    newUserDisconnect.text = '님이 퇴장하셨습니다.';
+
+    await this.chatRepository.save(newUserDisconnect);
+    socket.broadcast.emit('user_disconnect', socket.id);
   }
 
   @SubscribeMessage('chatting')
-  async handleChatting(@MessageBody() data, @ConnectedSocket() socket: Socket) {
+  async handleChatting(
+    @MessageBody() chat: CreateChatDto,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const chatting = new chatEntity();
+    chatting.username = chat.username;
+    chatting.text = chat.text;
+
+    await this.chatRepository.save(chatting);
+
     socket.broadcast.emit('chatting', {
       id: socket.id,
-      text: data,
+      text: chat,
     });
-    return 'Hello world!';
+  }
+
+  @SubscribeMessage('username')
+  async handleUserInfo(@MessageBody() data, @ConnectedSocket() socket: Socket) {
+    console.log(data);
   }
 }
--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatsController } from './chats.controller';
 import { ChatsGateway } from './chats.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatRepository } from './chats.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ChatRepository])],
   controllers: [ChatsController],
   providers: [ChatsGateway],
 })
