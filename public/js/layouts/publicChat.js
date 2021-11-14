bc6cba2d8e7dded9f456459ee8c38cd683d8b7da
FonDitbul <niki7084@naver.com>
2021-11-14 22:58:18 +0900

    공개 채팅방리스트 template 수정, 출력

---
+++ public/js/layouts/publicChat.js
@@ -0,0 +1,19 @@
+// const createRoomButton = document.getElementById('create-room-button');
+// const chatTable = document.querySelector('table');
+//
+// console.log(createRoomButton);
+// console.log(chatTable);
+//
+// function addChatTable() {
+//   const tr = table.insertRow();
+//
+//   const tdNumber = tr.insertCell(0);
+//   const tdRoomName = tr.insertCell(1);
+//   const createTime = tr.insertCell(2);
+//   const tdEntrance = tr.insertCell(3);
+//   const tdModify = tr.insertCell(4);
+// }
+//
+// // function
+//
+// addChatTable();
