318ca1fb947f637f824547eb65f6121b9ff5c103
FonDitbul <niki7084@naver.com>
2021-11-14 15:49:16 +0900

    chats socket 입, 퇴장 함수 parameter 변경, 기타 hbs 태그 변경

--- public/js/chats/chats.js
+++ public/js/chats/chats.js
@@ -1,17 +1,18 @@
 const socket = io('/chats');
 
 //socket on 리스트
-socket.on('user_entry', (username) => {
+socket.on('user_entry', (data) => {
   //신규 유저 접속
-  drawUserEntry(username);
+  drawUserDoor(data.username, data.text);
 });
-socket.on('user_exit', (username) => {
+socket.on('user_exit', (data) => {
   //유저 disconnect
-  drawUserExit(username);
+  drawUserDoor(data.username, data.text);
 });
-socket.on('chatting', (chat) => {
+socket.on('chatting', (data) => {
   //채팅 받았을 때
-  drawChattingContent(chat.id, chat.text);
+  console.log(data);
+  drawChattingContent(data.username, data.text);
 });
 
 //HTML Tage 받아오기
@@ -20,14 +21,9 @@ const userInfoForm = document.getElementById('user-info-form'); //유저 input t
 const chatInfoForm = document.getElementById('chat-info-form'); //채팅 input tag
 
 // draw 함수
-const drawUserEntry = (username) => {
+const drawUserDoor = (username, text) => {
   const textDiv = document.createElement('div');
-  textDiv.innerText = username + '님이 입장하셨습니다.';
-  chatlistDiv.appendChild(textDiv);
-};
-const drawUserExit = (username) => {
-  const textDiv = document.createElement('div');
-  textDiv.innerText = username + '님이 퇴장하셨습니다.';
+  textDiv.innerText = username + text;
   chatlistDiv.appendChild(textDiv);
 };
 const drawChattingContent = (id, text) => {
@@ -42,7 +38,9 @@ const onUserFormSubmit = (event) => {
   event.preventDefault();
   const inputValue = event.target.elements[0].value;
   sessionStorage.setItem('username', inputValue);
-  socket.emit('username', inputValue);
+  socket.emit('username', {
+    username: sessionStorage.getItem('username'),
+  });
   event.target.elements[0].value = '';
 };
 
@@ -61,3 +59,5 @@ const onChatFormSubmit = (event) => {
 //Form event 추가
 userInfoForm.addEventListener('submit', onUserFormSubmit);
 chatInfoForm.addEventListener('submit', onChatFormSubmit);
+
+const initRender = () => {};
