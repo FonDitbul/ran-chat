c9933ec59630ed39bb57cba4c640a56d53e55e4e
FonDitbul <niki7084@naver.com>
2021-11-17 00:53:13 +0900

    public room 채팅방 생성 client 코드

--- public/js/layouts/publicChat.js
+++ public/js/layouts/publicChat.js
@@ -1,16 +1,37 @@
-const createRoomButton = document.getElementById('create-room-button');
+const createModalButton = document.getElementById('create-modal-button');
 const chatTable = document.querySelector('table');
+const createRoomModal = document.getElementById('create-room-modal');
+const closeButton = document.getElementById('modal-close');
+const createRoomform = document.getElementById('create-room-form');
 
-async function createPublicRoom() {
-  const title = '생성테스트';
-  const uid = 1;
+const createPublicRoom = async (event) => {
+  event.preventDefault();
+  const title = event.target.elements[0].value;
+  const userName = sessionStorage.getItem('userName');
+  const response = await axios.get('/users/' + userName);
+  const uid = response.data.id;
+
   await axios.post('/public-chat', {
     title: title,
     uid: uid,
   });
   location.reload();
-}
-// Modal 생성 Click 이벤트로 parameter 받기
-createRoomButton.addEventListener('click', createPublicRoom);
+};
+createPublicRoom().then((r) => {
+  console.log(r);
+});
+createRoomform.addEventListener('submit', createPublicRoom);
 
-console.log(chatTable);
+createModalButton.onclick = function () {
+  createRoomModal.style.display = 'block';
+};
+
+closeButton.onclick = function () {
+  createRoomModal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == createRoomModal) {
+    createRoomModal.style.display = 'none';
+  }
+};
