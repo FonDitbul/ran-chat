31c363371bf2998fb037ffdb9e0834c832e331eb
FonDitbul <niki7084@naver.com>
2021-11-23 22:52:49 +0900

    timezone 수정, createdTime 포맷 함수

--- src/public-chat/public-chat.service.ts
+++ src/public-chat/public-chat.service.ts
@@ -25,9 +25,41 @@ export class PublicChatService {
   }
 
   async findAll() {
-    const getAllChat = getRepository(PublicChat)
+    const getAllChat = await getRepository(PublicChat)
       .createQueryBuilder('publicChat')
       .getMany();
+    function getDate(date) {
+      const year = (date.getFullYear() - 2000).toString();
+      const month: number = date.getMonth() + 1;
+      const day: number = date.getDate();
+      const hours = date.getHours(); // 시
+      const minutes = date.getMinutes(); // 분
+      const seconds = date.getSeconds(); // 초
+      const monthStr: string =
+        month < 10 ? '0' + month.toString() : month.toString();
+      const dayStr: string = day < 10 ? '0' + day.toString() : day.toString();
+      const hoursStr: string =
+        hours < 10 ? '0' + hours.toString() : hours.toString();
+      const minutesStr: string =
+        minutes < 10 ? '0' + minutes.toString() : minutes.toString();
+      const secondsStr: string =
+        seconds < 10 ? '0' + seconds.toString() : seconds.toString();
+      return (
+        year +
+        '/' +
+        monthStr +
+        '/' +
+        dayStr +
+        ' [' +
+        hoursStr +
+        ':' +
+        minutesStr +
+        ']'
+      );
+    }
+    for (const list of getAllChat) {
+      list['createdTime'] = getDate(list.createdAt);
+    }
     return getAllChat;
   }
 
