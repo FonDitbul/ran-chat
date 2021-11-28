7cb937bf5ffaeabd6ac01c7b4d0bf2db1361a54f
FonDitbul <niki7084@naver.com>
2021-11-29 00:23:17 +0900

    게시판 생성 dto 파일 추가

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,1 +1,16 @@
-export class CreateBoardDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateBoardDto {
+  @IsNotEmpty()
+  readonly title: string;
+
+  readonly category: number;
+
+  @IsNotEmpty()
+  readonly uid: number;
+
+  readonly content: string;
+
+  // @IsNotEmpty()
+  // readonly date;
+}
