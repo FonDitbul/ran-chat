d19834f29eaef0098a5e0dcc027909c3f9c00d0f
FonDitbul <niki7084@naver.com>
2021-11-30 18:25:51 +0900

    public-chat, board entity 변경에 따른 변수 변경, createBoard 추가

--- public/js/layouts/boards.js
+++ public/js/layouts/boards.js
@@ -1,5 +1,6 @@
 const createBoardButton = document.getElementById('create-board-button');
 const createBoardModal = document.getElementById('create-board-modal');
+const createBoardForm = document.getElementById('create-board-form');
 
 createBoardButton.onclick = function () {
   createBoardModal.style.display = 'block';
@@ -9,3 +10,19 @@ window.onclick = function (event) {
     createBoardModal.style.display = 'none';
   }
 };
+
+const createBoard = async (event) => {
+  event.preventDefault();
+  const title = event.target.elements[0].value;
+  const userName = sessionStorage.getItem('userName');
+  const response = await axios.get('/users/' + userName);
+  const uid = response.data.id;
+  await axios.post('/board', {
+    title: title,
+    category: 1,
+    uid: uid,
+    content: '테스트 게시판입니다 123 123',
+  });
+  return location.reload();
+};
+createBoardForm.addEventListener('submit', createBoard);
