c92210486b63168de1606d777b6595adfac55053
FonDitbul <niki7084@naver.com>
2021-12-02 16:21:52 +0900

    comment repository 생성

--- src/boards/boards.repository.ts
+++ src/boards/boards.repository.ts
@@ -1,9 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { BoardEntity } from './entities/board.entity';
-import { CommentEntity } from './entities/comment.entity';
 
 @EntityRepository(BoardEntity)
 export class BoardsRepository extends Repository<BoardEntity> {}
-
-@EntityRepository(CommentEntity)
-export class CommentRepository extends Repository<CommentEntity> {}
