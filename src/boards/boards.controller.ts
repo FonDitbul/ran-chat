b54ca508815b3b919c9fc9b347f411fa85e1f2f2
FonDitbul <niki7084@naver.com>
2021-12-03 14:10:07 +0900

    board controller, comment service 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -2,31 +2,37 @@ import {
   Controller,
   Get,
   Post,
-  Body,
   Patch,
-  Param,
   Delete,
+  Body,
+  Param,
   Render,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { UpdateLikeDto } from './dto/update-like.dto';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-board.dto';
+import { CommentService } from './services/comment.service';
 
 @Controller('board')
 export class BoardsController {
-  constructor(private readonly boardsService: BoardsService) {}
+  constructor(
+    private readonly boardsService: BoardsService,
+    private readonly commentService: CommentService,
+  ) {}
 
   @Get('')
   @Render('layouts/board')
-  async boardsPage() {
+  async allBoardsPage() {
     const boardList = await this.boardsService.findAll();
     return { title: '자유 게시판', data: boardList };
   }
 
   @Get(':id')
   @Render('template/boards')
-  async boardPage(@Param('id') id: string) {
+  async oneBoardPage(@Param('id') id: string) {
     const board = await this.boardsService.findOne(+id);
     return { title: '게시판 test', data: board };
   }
@@ -46,12 +52,27 @@ export class BoardsController {
     return this.boardsService.update(+id, updateBoardDto);
   }
 
-  @Patch('like/:id')
-  updateLike(@Param('id') id: string, @Body() updateLikeDto: UpdateLikeDto) {
-    return this.boardsService.updateLike(+id, updateLikeDto);
-  }
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.boardsService.remove(+id);
+  }
+
+  @Post('comment/:id')
+  createComment(
+    @Param('id') id: string,
+    @Body() createCommentDto: CreateCommentDto,
+  ) {
+    return this.commentService.create(createCommentDto);
+  }
+
+  @Patch('comment/:id')
+  updateComment(
+    @Param('id') id: string,
+    @Body() updateCommentDto: UpdateCommentDto,
+  ) {}
+
+  @Patch('like/:id')
+  updateLike(@Param('id') id: string, @Body() updateLikeDto: UpdateLikeDto) {
+    return this.boardsService.updateLike(+id, updateLikeDto);
   }
 }
