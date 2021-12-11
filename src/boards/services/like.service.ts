767a6a7dec97b8c4c1b18139f321a8e2d3844858
FonDitbul <niki7084@naver.com>
2021-12-11 21:46:43 +0900

    좋아요 서비스 업데이트

--- src/boards/services/like.service.ts
+++ src/boards/services/like.service.ts
@@ -7,13 +7,14 @@ import { BoardEntity as Board } from '../entities/board.entity';
 @Injectable()
 export class LikeService {
   constructor(private readonly boardsRepository: BoardsRepository) {}
-  async updateLike(id: number, updateLikeDto: UpdateLikeDto) {
-    let { like, dislike } = updateLikeDto;
+  async update(updateLikeDto: UpdateLikeDto) {
+    const { boardID, like, dislike } = updateLikeDto;
     const updateOneBoard = await getRepository(Board)
       .createQueryBuilder()
       .update('board')
-      .set({ like: ++like, dislike: ++dislike })
-      .where('board.id = :id', { id })
+      .set({ like: like, dislike: dislike })
+      .where('board.id = :id', { boardID })
       .execute();
+    return updateOneBoard;
   }
 }
