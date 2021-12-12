8ec6ac9fff7fb2a12d877d662d0b70d047a8dbfb
FonDitbul <niki7084@naver.com>
2021-12-12 22:43:35 +0900

    login  서버 데이터 연동

--- public/js/layouts/login.js
+++ public/js/layouts/login.js
@@ -4,11 +4,18 @@ const loginPassword = document.getElementById('login-password');
 const loginButton = document.getElementById('login-button');
 
 const submitLogin = async () => {
-  const uid = loginId.value;
-  if (!uid) {
+  const loginID = loginId.value;
+  if (!loginID) {
     return alert('유저 아이디를 입력해 주세요!');
   }
-  sessionStorage.setItem('userName', uid);
+  const response = await axios.get('/users/' + loginID);
+  const uid = response.data.id;
+  const userName = response.data.userName;
+  if (!response.data) {
+    return alert('존재하지 않는 아이디입니다!');
+  }
+  sessionStorage.setItem('uid', uid);
+  sessionStorage.setItem('userName', userName);
   return window.location.replace('/');
 };
 
@@ -30,14 +37,21 @@ window.onclick = function (event) {
 
 const submitSignup = async (event) => {
   event.preventDefault();
-  const uid = event.target.elements[0].value;
+  const userName = event.target.elements[0].value;
   const password = event.target.elements[1].value;
   const passwordConfirm = event.target.elements[2].value;
-  if (!uid) {
+  if (!userName) {
     return alert('유저 아이디를 입력해 주세요!');
   }
-
-  sessionStorage.setItem('userName', uid);
+  if (password !== passwordConfirm)
+    return alert('비밀번호가 동일하지 않습니다!');
+  const register = await axios.post('/users', {
+    userName: userName,
+    password: password,
+  });
+  const loginID = await axios.get('/users/' + userName);
+  sessionStorage.setItem('uid', loginID.data.id);
+  sessionStorage.setItem('userName', loginID.data.userName);
   return (window.location.href = '/');
 };
 const signUpform = document.getElementById('signup-form');
