8c02492c6daa4d338fab79c2e85000b0e31fe9ce
FonDitbul <niki7084@naver.com>
2021-12-21 17:16:29 +0900

    repository custom Logger, repository 적용

--- src/boards/repository/comment.repository.ts
+++ src/boards/repository/comment.repository.ts
@@ -1,5 +1,19 @@
-import { EntityRepository, Repository } from 'typeorm';
+import {
+  createConnection,
+  EntityRepository,
+  getConnectionOptions,
+  Repository,
+} from 'typeorm';
 import { CommentEntity } from '../entities/comment.entity';
+import { MyCustomLogger } from '../../common/typeorm/MyCustomLogger';
 
 @EntityRepository(CommentEntity)
 export class CommentRepository extends Repository<CommentEntity> {}
+
+getConnectionOptions().then((connectionOptions) => {
+  return createConnection(
+    Object.assign(connectionOptions, {
+      logger: new MyCustomLogger(),
+    }),
+  );
+});
---
+++ src/common/typeorm/MyCustomLogger.ts
@@ -0,0 +1,41 @@
+import { Logger, QueryRunner } from 'typeorm';
+
+export class MyCustomLogger implements Logger {
+  log(
+    level: 'log' | 'info' | 'warn',
+    message: any,
+    queryRunner?: QueryRunner,
+  ): any {}
+
+  logMigration(message: string, queryRunner?: QueryRunner): any {}
+
+  logQuery(query: string, parameters?: any[], queryRunner?: QueryRunner): any {
+    console.log(query);
+    /*    const queryList = query.split(',');
+    const reservedWord = ['SELECT', 'FROM'];
+    for (const queries of queryList) {
+      if (queries in reservedWord) {
+        console.log(`%c${queries}`, 'color: #a4f644; background-color: #000;');
+      } else {
+        console.log(queries);
+      }
+    }*/
+  }
+
+  logQueryError(
+    error: string | Error,
+    query: string,
+    parameters?: any[],
+    queryRunner?: QueryRunner,
+  ): any {}
+
+  logQuerySlow(
+    time: number,
+    query: string,
+    parameters?: any[],
+    queryRunner?: QueryRunner,
+  ): any {}
+
+  logSchemaBuild(message: string, queryRunner?: QueryRunner): any {}
+  // implement all methods from logger class
+}
