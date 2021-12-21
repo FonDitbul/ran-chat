49cd616ebba102583477e7957dd519117ee739cd
FonDitbul <niki7084@naver.com>
2021-12-21 22:53:46 +0900

    게시판 생성 페이지 button 변경, modal 삭제

--- public/js/layouts/boards.js
+++ public/js/layouts/boards.js
@@ -1,28 +1,26 @@
-const createBoardButton = document.getElementById('create-board-button');
-const createBoardModal = document.getElementById('create-board-modal');
-const createBoardForm = document.getElementById('create-board-form');
-
-createBoardButton.onclick = function () {
-  createBoardModal.style.display = 'block';
-};
-window.onclick = function (event) {
-  if (event.target == createBoardModal) {
-    createBoardModal.style.display = 'none';
-  }
-};
-
-const createBoard = async (event) => {
-  event.preventDefault();
-  const title = event.target.elements[0].value;
-  const userName = sessionStorage.getItem('userName');
-  const response = await axios.get('/users/' + userName);
-  const uid = response.data.id;
-  await axios.post('/board', {
-    title: title,
-    category: 1,
-    uid: uid,
-    content: '테스트 게시판입니다 123 123',
-  });
-  return location.reload();
-};
-createBoardForm.addEventListener('submit', createBoard);
+// const createBoardButton = document.getElementById('create-board-button');
+//
+// createBoardButton.onclick = function () {
+//   createBoardModal.style.display = 'block';
+// };
+// window.onclick = function (event) {
+//   if (event.target == createBoardModal) {
+//     createBoardModal.style.display = 'none';
+//   }
+// };
+//
+// const createBoard = async (event) => {
+//   event.preventDefault();
+//   const title = event.target.elements[0].value;
+//   const userName = sessionStorage.getItem('userName');
+//   const response = await axios.get('/users/' + userName);
+//   const uid = response.data.id;
+//   await axios.post('/board', {
+//     title: title,
+//     category: 1,
+//     uid: uid,
+//     content: '테스트 게시판입니다 123 123',
+//   });
+//   return location.reload();
+// };
+// createBoardForm.addEventListener('submit', createBoard);
