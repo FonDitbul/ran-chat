bf6dcb09399734e4d2c37316cc9a405b1beef2d1
FonDitbul <niki7084@naver.com>
2021-12-21 21:47:24 +0900

    summernote 라이브러리 적용

---
+++ public/js/template/createBoard.js
@@ -0,0 +1,14 @@
+$('#summernote').summernote({
+  placeholder: 'Hello Bootstrap 4',
+  tabsize: 2,
+  height: 600,
+  toolbar: [
+    ['style', ['style']],
+    ['font', ['bold', 'underline', 'clear']],
+    ['color', ['color']],
+    ['para', ['ul', 'ol', 'paragraph']],
+    ['table', ['table']],
+    ['insert', ['link', 'picture', 'video']],
+    ['view', ['fullscreen', 'codeview', 'help']],
+  ],
+});
