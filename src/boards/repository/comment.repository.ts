bc5bf2c5e9f6b67750d1dff59125cbf6e1122c21
FonDitbul <niki7084@naver.com>
2021-12-24 22:43:47 +0900

    service, repository 리팩터링

--- src/boards/repository/comment.repository.ts
+++ src/boards/repository/comment.repository.ts
@@ -2,13 +2,69 @@ import {
   createConnection,
   EntityRepository,
   getConnectionOptions,
+  getRepository,
   Repository,
 } from 'typeorm';
-import { CommentEntity } from '../entities/comment.entity';
+import {
+  CommentEntity as Comment,
+  CommentEntity,
+} from '../entities/comment.entity';
 import { MyCustomLogger } from '../../common/typeorm/MyCustomLogger';
+import { UserEntity } from '../../users/entities/user.entity';
+import { UpdateCommentDto } from '../dto/update-comment.dto';
 
 @EntityRepository(CommentEntity)
-export class CommentRepository extends Repository<CommentEntity> {}
+export class CommentRepository extends Repository<CommentEntity> {
+  async findAllBoard(boardID: number) {
+    const getAllComment = await getRepository(Comment)
+      .createQueryBuilder('comment')
+      .addSelect((subQuery) => {
+        return subQuery
+          .select(['user.userName'])
+          .from(UserEntity, 'user')
+          .where('comment.uid = user.id')
+          .limit(1);
+      }, 'user_userName')
+      .andWhere('comment.boardID = :boardID', { boardID })
+      .andWhere('comment.groupID = 0')
+      .getRawMany();
+    return getAllComment;
+  }
+
+  async findOneBoard(boardID: number, id: number) {
+    const getOneComment = await getRepository(Comment)
+      .createQueryBuilder('comment')
+      .andWhere('comment.id = :id', { id })
+      .andWhere('comment.boardID = :boardID', { boardID })
+      .getRawMany();
+    return getOneComment;
+  }
+  async updateBoard(updateCommentDto: UpdateCommentDto) {
+    const { id, boardID, content } = updateCommentDto;
+
+    const updateComment = await getRepository(Comment)
+      .createQueryBuilder('comment')
+      .update('comment')
+      .set({ content: content })
+      .andWhere('comment.id = :id', { id })
+      .andWhere('comment.boardID = :boardID', { boardID });
+    return updateComment;
+  }
+  async findReplyComment(groupID: number) {
+    const replyComment = await getRepository(Comment)
+      .createQueryBuilder('comment')
+      .addSelect((subQuery) => {
+        return subQuery
+          .select(['user.userName'])
+          .from(UserEntity, 'user')
+          .where('comment.uid = user.id')
+          .limit(1);
+      }, 'user_userName')
+      .andWhere('comment.groupID = :groupID', { groupID })
+      .getRawMany();
+    return replyComment;
+  }
+}
 
 getConnectionOptions().then((connectionOptions) => {
   return createConnection(
--- src/boards/services/comment.service.ts
+++ src/boards/services/comment.service.ts
@@ -2,26 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { CommentRepository } from '../repository/comment.repository';
 import { CreateCommentDto } from '../dto/create-comment.dto';
 import { UpdateCommentDto } from '../dto/update-comment.dto';
-import { getRepository } from 'typeorm';
-import { CommentEntity as Comment } from '../entities/comment.entity';
-import { UserEntity } from '../../users/entities/user.entity';
 
 @Injectable()
 export class CommentService {
   constructor(private readonly commentRepository: CommentRepository) {}
+
   async findAll(boardID: number) {
-    const getAllComment = await getRepository(Comment)
-      .createQueryBuilder('comment')
-      .addSelect((subQuery) => {
-        return subQuery
-          .select(['user.userName'])
-          .from(UserEntity, 'user')
-          .where('comment.uid = user.id')
-          .limit(1);
-      }, 'user_userName')
-      .andWhere('comment.boardID = :boardID', { boardID })
-      .andWhere('comment.groupID = 0')
-      .getRawMany();
+    const getAllComment = await this.commentRepository.findAllBoard(boardID);
     for (const reply of getAllComment) {
       reply['comment_reply'] = await this.findReplyComment(reply.comment_id);
     }
@@ -29,12 +16,7 @@ export class CommentService {
   }
 
   async findOne(boardID: number, id: number) {
-    const getOneComment = await getRepository(Comment)
-      .createQueryBuilder('comment')
-      .andWhere('comment.id = :id', { id })
-      .andWhere('comment.boardID = :boardID', { boardID })
-      .getRawMany();
-    return getOneComment;
+    return this.commentRepository.findOneBoard(boardID, id);
   }
 
   async create(createCommentDto: CreateCommentDto) {
@@ -49,33 +31,13 @@ export class CommentService {
   }
 
   async update(updateCommentDto: UpdateCommentDto) {
-    const { id, boardID, content } = updateCommentDto;
-
-    const updateComment = await getRepository(Comment)
-      .createQueryBuilder('comment')
-      .update('comment')
-      .set({ content: content })
-      .andWhere('comment.id = :id', { id })
-      .andWhere('comment.boardID = :boardID', { boardID });
-    return updateComment;
+    return await this.commentRepository.updateBoard(updateCommentDto);
   }
 
   async remove(id: number) {
-    const removedData = await this.commentRepository.delete({ id: id });
-    return 'remove';
+    return await this.commentRepository.delete({ id: id });
   }
   async findReplyComment(groupID: number) {
-    const replyComment = await getRepository(Comment)
-      .createQueryBuilder('comment')
-      .addSelect((subQuery) => {
-        return subQuery
-          .select(['user.userName'])
-          .from(UserEntity, 'user')
-          .where('comment.uid = user.id')
-          .limit(1);
-      }, 'user_userName')
-      .andWhere('comment.groupID = :groupID', { groupID })
-      .getRawMany();
-    return replyComment;
+    return await this.commentRepository.findReplyComment(groupID);
   }
 }
