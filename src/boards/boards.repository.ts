7ffe93b48e4ce4bfea5ca6904d6c4600a0e7d490
FonDitbul <niki7084@naver.com>
2022-01-15 16:44:33 +0900

    getRepository를 this로 변경(불필요), id 컬럼 참조 모호로 인한 표현식 변경

--- src/boards/boards.repository.ts
+++ src/boards/boards.repository.ts
@@ -1,49 +1,23 @@
 import { EntityRepository, getRepository, Repository } from 'typeorm';
 import { BoardEntity as Board, BoardEntity } from './entities/board.entity';
-import { UserEntity as User, UserEntity } from '../users/entities/user.entity';
-import { CommentEntity } from './entities/comment.entity';
+import { UserEntity as User } from '../users/entities/user.entity';
 import { UpdateBoardDto } from './dto/update-board.dto';
 
 @EntityRepository(BoardEntity)
 export class BoardsRepository extends Repository<BoardEntity> {
   async findAll(page: number) {
     const SHOW_LIMIT_BOARD = 10;
-    const getAllBoards = await getRepository(Board)
-      .createQueryBuilder('board')
-      .select([
-        'board.id',
-        'board.title',
-        'board.uid', // 게시판 작성유저 아이디
-        'board.category', // 게시판 카테고리
-        'board.createdAt', //생성 날짜
-        'board.like', // 좋아요
-        'board.dislike', // 싫어요
-        'board.views', //조회수
-      ])
-      // .leftJoin('board', 'user', 'board.uid = user.id')
-      .addSelect((subQuery) => {
-        return subQuery
-          .select(['user.userName'])
-          .from(UserEntity, 'user')
-          .where('board.uid = user.id')
-          .limit(1);
-      }, 'user_userName')
-      .addSelect((subQuery) => {
-        return subQuery
-          .select('COUNT(*) AS comment_count')
-          .from(CommentEntity, 'comment')
-          .where('board.id = comment.boardID');
-      }, 'comment_count')
-      .orderBy('id', 'DESC')
-      .offset(page * SHOW_LIMIT_BOARD)
-      .limit(SHOW_LIMIT_BOARD)
-      .getMany();
+    const getAllBoards = await this.find({
+      relations: ['user'],
+      order: { id: 'DESC' },
+      skip: page * SHOW_LIMIT_BOARD,
+      take: SHOW_LIMIT_BOARD,
+    });
     return getAllBoards;
   }
 
   async findOneBoard(id: number) {
-    const getOneBoard = await getRepository(Board)
-      .createQueryBuilder('board')
+    const getOneBoard = await this.createQueryBuilder('board')
       .select([
         'board.id',
         'board.title',
@@ -61,8 +35,7 @@ export class BoardsRepository extends Repository<BoardEntity> {
   }
 
   async findLike(id: number) {
-    const boardLikes = await getRepository(Board)
-      .createQueryBuilder('board')
+    const boardLikes = await this.createQueryBuilder('board')
       .leftJoinAndSelect('board.userLikes', 'Like')
       .where('board.id = :id', { id })
       .getOne();
@@ -71,8 +44,7 @@ export class BoardsRepository extends Repository<BoardEntity> {
 
   async updateBoard(id: number, updateBoardDto: UpdateBoardDto) {
     const { content } = updateBoardDto;
-    const updateOneBoard = await getRepository(Board)
-      .createQueryBuilder()
+    const updateOneBoard = await this.createQueryBuilder()
       .update('board')
       .set({ content: content })
       .where('board.id = :id', { id })
@@ -87,8 +59,7 @@ export class BoardsRepository extends Repository<BoardEntity> {
       .where('user.id = :uid', { uid })
       .getOne();
 
-    const board = await getRepository(Board)
-      .createQueryBuilder('board')
+    const board = await this.createQueryBuilder('board')
       .leftJoinAndSelect('board.userLikes', 'Like')
       .where('board.id = :id', { id })
       .getOne();
