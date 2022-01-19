6c511a5080dc1d42a27da5d69eb8db5e90df795b
FonDitbul <niki7084@naver.com>
2022-01-19 21:34:37 +0900

    회원가입 dto 수정

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,16 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { UserEntity } from '../entities/user.entity';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto extends PickType(UserEntity, [
   'userName',
   'password',
-] as const) {}
+] as const) {
+  @ApiProperty({
+    example: '1q2w3e4r',
+    description: '비밀번호 확인',
+    required: true,
+  })
+  @IsNotEmpty()
+  passwordConfirm: string;
+}
