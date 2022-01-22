ab7849b14b8fec4b51275bf90511a52b218ffd52
FonDitbul <niki7084@naver.com>
2022-01-22 00:10:59 +0000

    boards 게시판 생성 로그인 x 막기

--- public/js/layouts/boards.js
+++ public/js/layouts/boards.js
@@ -1,26 +1,8 @@
-// const createBoardButton = document.getElementById('create-board-button');
-//
-// createBoardButton.onclick = function () {
-//   createBoardModal.style.display = 'block';
-// };
-// window.onclick = function (event) {
-//   if (event.target == createBoardModal) {
-//     createBoardModal.style.display = 'none';
-//   }
-// };
-//
-// const createBoard = async (event) => {
-//   event.preventDefault();
-//   const title = event.target.elements[0].value;
-//   const userName = sessionStorage.getItem('userName');
-//   const response = await axios.get('/users/' + userName);
-//   const uid = response.data.id;
-//   await axios.post('/board', {
-//     title: title,
-//     category: 1,
-//     uid: uid,
-//     content: '테스트 게시판입니다 123 123',
-//   });
-//   return location.reload();
-// };
-// createBoardForm.addEventListener('submit', createBoard);
+const createBoardButton = document.getElementById('create-board');
+
+const createBoard = async () => {
+  const access_token = localStorage.getItem('access_token');
+  if (!access_token) return alert('로그인 후 이용해 주세요.');
+  return window.location.replace('/board/create');
+};
+createBoardButton.addEventListener('click', createBoard);
