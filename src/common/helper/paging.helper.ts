6f71ca95f3671293b2b85f5512fabe4684793f3b
FonDitbul <niki7084@naver.com>
2022-02-01 14:19:25 +0900

    일부 변수 수정, endPage가 < 10 일때 startPage 예외처리

--- src/common/helper/paging.helper.ts
+++ src/common/helper/paging.helper.ts
@@ -1,14 +1,17 @@
 const SHOW_BOARD = 15; // 게시판 개수 (서버와 동일한 변수)
 const SHOW_PAGING_NUM = 10; // PAGING 개수
 
-export const pageToArray = (curPage: number, totalCountBoard: number) => {
-  const totalPage = Math.ceil(totalCountBoard / SHOW_BOARD); //총 페이지 개수
+export const pageToArray = (curPage: number, totalCount: number) => {
+  const totalPage = Math.ceil(totalCount / SHOW_BOARD); //총 페이지 개수
   const endPage: number = //페이지 끝 번호
     totalPage < Math.ceil(curPage / SHOW_PAGING_NUM) * SHOW_PAGING_NUM
       ? totalPage
       : Math.ceil(curPage / SHOW_PAGING_NUM) * SHOW_PAGING_NUM;
-  const startPage: number = endPage - (SHOW_PAGING_NUM - 1); //페이지 시작 번호
-
+  let startPage: number = endPage - (SHOW_PAGING_NUM - 1); //페이지 시작 번호
+  if (Math.ceil(startPage / SHOW_PAGING_NUM) <= 0) {
+    //1일때 예외처리
+    startPage = 1;
+  }
   const pageArr: number[] = []; //페이지 시작 ~ 끝 배열 반환
   for (let i = startPage; i <= endPage; i++) {
     pageArr.push(i);
@@ -26,7 +29,7 @@ export const preCurPage = (curPage) => {
   return curPage - 1;
 };
 
-export const nextCurPage = (curPage, totalCountBoard: number) => {
-  if (Math.ceil(totalCountBoard / SHOW_BOARD) <= curPage) return curPage;
+export const nextCurPage = (curPage, totalCount: number) => {
+  if (Math.ceil(totalCount / SHOW_BOARD) <= curPage) return curPage;
   return curPage + 1;
 };
