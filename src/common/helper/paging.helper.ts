f9fc971cf3465f624e6decd0a8c4e66a775febbc
FonDitbul <niki7084@naver.com>
2022-02-01 11:47:10 +0900

    paging 완료

--- src/common/helper/paging.helper.ts
+++ src/common/helper/paging.helper.ts
@@ -1,22 +1,18 @@
-const SHOW_BOARD = 15;
-const SHOW_PAGING_NUM = 10;
+const SHOW_BOARD = 15; // 게시판 개수 (서버와 동일한 변수)
+const SHOW_PAGING_NUM = 10; // PAGING 개수
 
 export const pageToArray = (curPage: number, totalCountBoard: number) => {
-  const startPage: number =
-    Math.floor(curPage / SHOW_PAGING_NUM) * SHOW_PAGING_NUM;
-  const endPage: number =
-    Math.ceil(totalCountBoard / SHOW_BOARD) <
-    Math.ceil(startPage * SHOW_PAGING_NUM)
-      ? Math.ceil(totalCountBoard / SHOW_BOARD)
-      : Math.ceil(startPage * SHOW_PAGING_NUM);
+  const totalPage = Math.ceil(totalCountBoard / SHOW_BOARD); //총 페이지 개수
+  const endPage: number = //페이지 끝 번호
+    totalPage < Math.ceil(curPage / SHOW_PAGING_NUM) * SHOW_PAGING_NUM
+      ? totalPage
+      : Math.ceil(curPage / SHOW_PAGING_NUM) * SHOW_PAGING_NUM;
+  const startPage: number = endPage - (SHOW_PAGING_NUM - 1); //페이지 시작 번호
 
-  const pageArr: number[] = [];
+  const pageArr: number[] = []; //페이지 시작 ~ 끝 배열 반환
   for (let i = startPage; i <= endPage; i++) {
     pageArr.push(i);
   }
-  // console.log('startpage', startPage);
-  // console.log(curPage / SHOW_PAGING_NUM); // 임시 startPage
-  // console.log(endPage);
   return pageArr;
 };
 
