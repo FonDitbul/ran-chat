c885d8e1a7763be7875018567b8fee0ef17e64cd
FonDitbul <niki7084@naver.com>
2022-02-03 16:21:51 +0900

    socket 버전 변경, 랜덤채팅 front 업데이트

--- public/js/layouts/randomChat.js
+++ public/js/layouts/randomChat.js
@@ -1,27 +1,25 @@
 (() => {
-  const socket = io('/chats');
+  const socket = io('/random-chat');
   const MESSAGE_DIV = document.getElementById('message');
-
+  let randomUser = '';
   //socket on 리스트
   socket.on('randomUserEntry', (data) => {
-    //랜덤 유저 매칭 성공
-    return MESSAGE_DIV.appendChild(
-      drawChattingContent('상대방님이 입장하셨습니다.'),
-    );
+    //랜덤 유저 매칭 성공 출력
+    randomUser = data.room;
+    return MESSAGE_DIV.appendChild(drawChattingContent('대화를 나눠보쎼용'));
   });
   socket.on('randomUserExit', (data) => {
-    //랜덤 유저 disconnect
+    //랜덤 유저 disconnect 출력
     return MESSAGE_DIV.appendChild(
       drawChattingContent('상대방님이 퇴장하셨습니다.'),
     );
   });
-  socket.on('chatting', (data) => {
-    //채팅 받았을 때
-    return drawChattingContent(data.userName, data.text);
+  socket.on('recMsg', function (res) {
+    //채팅 메시지를 받았을 때
+    return drawChattingMessage('상대방', res.text, MESSAGE_DIV);
   });
 
-  socket.emit('matchingUser', {});
-
+  //채팅 메시지 그리기
   const drawChattingMessage = (user, text, messageDivElement) => {
     const messageDiv = document.createElement('div');
     const innerDiv = document.createElement('div');
@@ -43,11 +41,12 @@
     messageDivElement.scrollTop = messageDivElement.scrollHeight;
     return messageDivElement;
   };
+  //채팅 시스템 내용 그리기
   const drawChattingContent = (text) => {
     const createDiv = document.createElement('div');
     const createSpan = document.createElement('span');
     createDiv.className =
-      'bg-gray-400 rounded px-4 py-2 my-2 text-white relative';
+      'bg-gray-400 rounded px-4 py-2 my-2 text-white justify-center';
     createSpan.innerText = text;
     createDiv.appendChild(createSpan);
     return createDiv;
@@ -55,12 +54,14 @@
 
   // 랜덤채팅 시작하기 버튼
   const ranChatMatchButton = document.getElementById('ran-chat-match');
-  const onClickMatchButton = (event) => {
+  const onClickMatchButton = async (event) => {
     event.target.innerText = '... 매칭을 기다리는 중 ...';
     const chattingForm =
       event.target.parentElement.getElementsByTagName('div')[0];
     visibleElement(chattingForm);
+    await socket.emit('matchingUser', {});
   };
+
   const visibleElement = (element) => {
     if (element.style.display === 'none')
       return (element.style.display = 'flex');
@@ -68,10 +69,18 @@
   };
   ranChatMatchButton.addEventListener('click', onClickMatchButton);
 
-  //랜덤 채팅 메세지 적기 div
+  //랜덤 채팅 메세지 적기
   const chatForm = document.getElementById('chat-form');
   const onClickChatForm = (event) => {
     event.preventDefault();
+    const text = event.target[0].value;
+    socket.emit('reqMsg', {
+      room: randomUser,
+      text: text,
+    });
+    drawChattingMessage('나', text, MESSAGE_DIV);
+    //채팅 초기화
+    event.target[0].value = '';
   };
-  chatForm.addEventListener('click', onClickChatForm);
+  chatForm.addEventListener('submit', onClickChatForm);
 })();
