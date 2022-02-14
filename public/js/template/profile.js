cfafcfbbf629ba543502c9254ff4c3472a43a00b
FonDitbul <niki7084@naver.com>
2022-02-14 20:04:55 +0900

    front) 계정 탈퇴 버튼 추가

--- public/js/template/profile.js
+++ public/js/template/profile.js
@@ -7,3 +7,11 @@ logoutButton.onclick = () => {
   localStorage.clear();
   return (window.location.href = '/');
 };
+
+const userDeleteButton = document.getElementById('user-delete-button');
+userDeleteButton.onclick = async () => {
+  const uid = localStorage.getItem('uid');
+  await axios.delete('/users/' + uid);
+  localStorage.clear();
+  return (window.location.href = '/');
+};
